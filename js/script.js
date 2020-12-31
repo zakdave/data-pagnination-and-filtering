33d50972ef5f86bbfe2d38609729d7da1016d4d5
zakdave <74794944+zakdave@users.noreply.github.com>
2020-12-31 11:11:46 -0500

    showPage()

--- js/script.js
+++ js/script.js
@@ -20,24 +20,32 @@ Create the `showPage` function
 */
 function showPage(list, page) {
    // create two variables which will represent the index for the first and last student on the page
-   const firstStudent = data[(page * 9) - 9];
-   const lastStudent = data[(page * 9) - 1];
-
-   console.log(firstStudent);
-   console.log(lastStudent);
+   const firstStudentIndex = (page * 9) - 9;
+   const lastStudentIndex = (page * 9) - 1;
 
    // select the element with a class of `student-list` and assign it to a variable
    const studentList = document.querySelector('.student-list');
    // set the innerHTML property of the variable you just created to an empty string
-   studentList.innerHTML = '';
+   studentList.innerHTML = ``;
    // loop over the length of the `list` parameter
-     // inside the loop create a conditional to display the proper students
-       // inside the conditional:
-         // create the elements needed to display the student information
-         // insert the above elements
- }
- 
-showPage(undefined, 1);
+   for (let i = firstStudentIndex; i <= lastStudentIndex; i++) {
+      let iterationString = 
+      `
+      <li class="student-item cf">
+         <div class="student-details">
+            <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+         </div>
+      <div class="joined-details">
+        <span class="date">${list[i].registered.date}</span>
+      </div>
+      </li>
+    `;
+    studentList.innerHTML += iterationString
+   }
+}
+showPage(data, 2);
 
 /*
 Create the `addPagination` function
